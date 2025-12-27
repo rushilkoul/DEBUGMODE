46b9698cbb6385f89ecdce2e04faef4a930de7df
Rushil Koul <ruskoul3@gmail.com>
2025-12-27 17:02:20 +0530

    Migrated to old input system, works in editor now.

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,39 +2,79 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    [Header("References")]
     public Rigidbody rb;
-    public Transform player;
-    public float Speed= 10f;
-    public float jforce= 10f;
+    public Transform orientation;
+
+    [Header("Movement Stats")]
+    public float moveSpeed = 4f;
+    public float jumpForce = 5f;
+    public float groundDrag = 6f;
+    public float airMultiplier = 0.4f;
+
+    [Header("Ground Detection")]
     public LayerMask groundLayer;
-    public float rayDistance;
-    private bool isGrounded;
-    public float drag= 0.3f;
+    public float rayDistance = 1.1f;
+    public bool isGrounded;
+
+    float horizontalInput;
+    float verticalInput;
+    Vector3 moveDirection;
+
+    void Start()
+    {
+        if (rb == null) rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
     void Update()
     {
         isGrounded = Physics.Raycast(transform.position, Vector3.down, rayDistance, groundLayer);
-        Color debugColor = isGrounded ? Color.green : Color.red;
-        Debug.DrawRay(transform.position, Vector3.down * rayDistance, debugColor);
+        
+        Debug.DrawRay(transform.position, Vector3.down * rayDistance, isGrounded ? Color.green : Color.red);
+
+        if (isGrounded)
+            rb.linearDamping = groundDrag;
+        else
+            rb.linearDamping = 0;
+
+        GetInput();
+    }
+
+    void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void GetInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
 
         if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             Jump();
         }
-       Move(); 
     }
-    void Move()
+
+    void MovePlayer()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal"); 
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        Vector3 dir = (player.forward * vertical) + (player.right * horizontal);
-
-        float cForce = isGrounded ? Speed : Speed * drag;
-        rb.AddForce(dir* cForce* Speed, ForceMode.Force);
+        moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+        if (isGrounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+        }
+        else
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+        }
     }
+
     void Jump()
     {
-        rb.linearVelocity = new Vector3(rb.linearVelocity.x, 0f, rb.linearVelocity.z);
-        rb.AddForce(Vector3.up * jforce, ForceMode.Impulse);
+        Vector3 vel = rb.linearVelocity; 
+        rb.linearVelocity = new Vector3(vel.x, 0f, vel.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
     }
 }
